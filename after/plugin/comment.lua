129926aa4508eaf8a779d444429bb9c544491a5f
Boris Rizov <boris.risov@gmail.com>
2023-01-17 21:20:33 +0100

    Update Comment.nvim setup

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,6 +1,11 @@
 require('Comment').setup({
     ignore = '^$',
+    -- this is for NORMAL mode
     toggler = {
+        line = '<C-_>'
+    },
+    -- this is for VISUAL mode
+    opleader = {
         line = '<C-_>'
     }
 })
