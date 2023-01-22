52f23a34502483af95750106e32bdbe819373fc1
Boris Rizov <boris.risov@gmail.com>
2023-01-22 17:43:45 +0100

    Add descriptions to LSP keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,31 +3,31 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'sumneko_lua',
-	'rust_analyzer',
+    'tsserver',
+    'eslint',
+    'sumneko_lua',
+    'rust_analyzer',
 })
 
 -- Fix Undefined global 'vim'
 lsp.configure('sumneko_lua', {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { 'vim' }
-			}
-		}
-	}
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 })
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 -- disable completion with tab
@@ -36,42 +36,42 @@ cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 lsp.set_preferences({
-	suggest_lsp_servers = false,
-	sign_icons = {
-		error = 'E',
-		warn = 'W',
-		hint = 'H',
-		info = 'I'
-	}
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
 })
 
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
+    local unpack = unpack or table.unpack
 
-	if client.name == "eslint" then
-		vim.cmd.LspStop('eslint')
-		return
-	end
+    if client.name == "eslint" then
+        vim.cmd.LspStop('eslint')
+        return
+    end
 
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-	vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-	vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-	vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = '[G]o to [D]efinition', unpack(opts) })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = 'Hover', unpack(opts) })
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, { desc = '[W]orkspace [S]ymbol', unpack(opts) })
+    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = 'Open [d]iagnostic float', unpack(opts) })
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, { desc = 'Go to next diagnostic', unpack(opts) })
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, { desc = 'Go to prev diagnostic', unpack(opts) })
+    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, { desc = '[C]ode [A]ction', unpack(opts) })
+    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, { desc = '[R]efe[r]ences', unpack(opts) })
+    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { desc = '[R]e[n]ame', unpack(opts) })
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = 'Signature [h]elp', unpack(opts) })
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-	virtual_text = true,
+    virtual_text = true,
 })
-
