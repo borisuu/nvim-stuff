443f926decd7f54c2ab9cb731ab9683da2cbfd0c
Boris Rizov <boris.risov@gmail.com>
2023-01-22 17:44:15 +0100

    Update standard keymaps [more]
    - Remove duplicate keymaps for LSP
    - Add descriptions to keymaps

--- lua/borisu/remap.lua
+++ lua/borisu/remap.lua
@@ -17,22 +17,22 @@ vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true })
 
 -- -- try this out for a while
-vim.keymap.set("n", "U", vim.cmd.redo)
+vim.keymap.set("n", "U", vim.cmd.redo, { desc = 'Redo' })
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = 'Copy to clipboard' })
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -40,16 +40,11 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = 'Rename word under cursor' })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = 'Make file executable', silent = true })
 
-vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
-vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
-vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
-vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
-
-vim.keymap.set("n", "<leader>di", vim.diagnostic.open_float)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-
-
+-- vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
+-- vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
+-- vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
+-- vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
